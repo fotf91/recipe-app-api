5ac539bcebe0dad12ea76000f182dce9c51f97e9
fotf91 <fotf91@gmail.com>
2021-04-20 12:27:27 +0300

    format files

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
@@ -8,8 +9,8 @@ class ModelTests(TestCase):
         email = 'test@gmail.com'
         password = 'Test123'
         user = get_user_model().objects.create_user(
-            email = email,
-            password = password
+            email=email,
+            password=password
         )
 
         self.assertEqual(user.email, email)
@@ -22,12 +23,10 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email.lower())
 
-
     def test_new_user_invalid_email(self):
         """Test creation of user with no emai, raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test123')
-
 
     def test_create_new_superuser(self):
         """Test creating a new superuser"""
